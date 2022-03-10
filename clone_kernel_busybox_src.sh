1fb2c006678e4c77903a43cc7948573ba0e0ffc3
ntneitin <39289849+ntneitin@users.noreply.github.com>
2022-03-10 14:07:25 +0530

    add script to clone upstream kernel and busybox

---
+++ clone_kernel_busybox_src.sh
@@ -0,0 +1,4 @@
+mkdir kernel_src;
+mkdir busybox;
+git clone -b linux-5.16.y https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git kernel_src;
+git clone https://github.com/mirror/busybox.git busybox;
