0d8d48a5189fee946c076e2df78ff82f8f117659
ntneitin <39289849+ntneitin@users.noreply.github.com>
2022-03-12 16:52:36 +0530

    Update clone_kernel_busybox_src.sh

--- clone_kernel_busybox_src.sh
+++ clone_kernel_busybox_src.sh
@@ -6,5 +6,5 @@ if [ ! -d "./busybox" ]; then
     mkdir -v busybox;
 fi
 
-git clone -b ntneitin-5.15 https://github.com/ntneitin/linux.git kernel_src;
-git clone -b ntneitin https://github.com/ntneitin/busybox.git busybox;
+git clone -b nt-5.15 https://github.com/ntneitin/linux.git kernel_src;
+git clone -b nt https://github.com/ntneitin/busybox.git busybox;
